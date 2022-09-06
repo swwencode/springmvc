41cdf72cb09e4b4f37a138fc8913b770fcdab30e
ZW150 <swwencode@qq.com>
2022-09-06 15:58:40 +0800

    新加一个测试类MyTest003

---
+++ springmvc_001/src/test/java/com/xxx/text/MyTest003.java
@@ -0,0 +1,10 @@
+package com.xxx.text;
+
+import org.junit.Test;
+
+public class MyTest003 {
+    @Test
+    public void testGitBatch(){
+        System.out.println("GitBatch............");
+    }
+}
